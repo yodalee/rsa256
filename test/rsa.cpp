172458277b59a83ce8239be222fb279aec68fb76
Yoda Lee <lc85301@gmail.com>
2022-10-23 09:36:17 +0800

    add montgomery c-model

--- test/rsa.cpp
+++ test/rsa.cpp
@@ -12,3 +12,24 @@ void two_power_mod(unsigned power, mpz_t N, mpz_t out) {
   }
   mpz_set(out, base);
 }
+
+void montgomery_base2(mpz_t A, mpz_t B, mpz_t N, mpz_t out) {
+  mpz_t round_result; // S in doc
+  mpz_init_set_ui(round_result, 0);
+
+  for (int i = 0; i < 256; ++i) {
+    bool bit_i = mpz_tstbit(A, i);
+    if (bit_i) {
+      mpz_add(round_result, round_result, B);
+    }
+    bool is_odd = mpz_tstbit(round_result, 0);
+    if (is_odd) {
+      mpz_add(round_result, round_result, N);
+    }
+    mpz_tdiv_q_ui(round_result, round_result, 2u);
+  }
+  if (mpz_cmp(round_result, N) > 0) {
+    mpz_sub(round_result, round_result, N);
+  }
+  mpz_set(out, round_result);
+}
--- test/rsa.h
+++ test/rsa.h
@@ -3,3 +3,8 @@
 
 // calculate ((2 ** power) mod n)
 void two_power_mod(unsigned power, mpz_t N, mpz_t out);
+
+// montgomery algorithm
+// Given two numbers A and B, and the mod number N
+// calculate (A * B * (2 ** -256)) mod N, 1 bit per round
+void montgomery_base2(mpz_t A, mpz_t B, mpz_t N, mpz_t out);
