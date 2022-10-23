64fdfffe68658687c9f9d54c9d12521cdde9230f
Yoda Lee <lc85301@gmail.com>
2022-10-23 10:27:17 +0800

    Add test for montegomery

--- test/test_rsa.cpp
+++ test/test_rsa.cpp
@@ -22,6 +22,23 @@ TEST(RsaTest, test_two_power_mod) {
   EXPECT_EQ(0, mpz_cmp(out,out_ans));
 }
 
+TEST(RsaTest, test_montgomery_to_self) {
+  // MA(A, 2 ** 256) = A
+  const char str_N[] = "0xE07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1";
+  const char str_A[] = "0x412820616369726641206874756F53202C48544542415A494C452054524F50";
+
+  mpz_t A, B, N, out;
+  mpz_init_set_str(A, str_A, 0);
+  mpz_init_set_str(N, str_N, 0);
+  mpz_inits(B, out, NULL);
+
+  two_power_mod(256, N, B);
+  montgomery_base2(A, B, N, out);
+
+  // check output
+  EXPECT_EQ(0, mpz_cmp(out, A));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
