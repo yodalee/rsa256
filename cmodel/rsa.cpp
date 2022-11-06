af95f8ebf97d485ba3cea1534a5e74ed8d674354
Yu-Sheng Lin <johnjohnlys@gmail.com>
2022-11-06 12:32:05 +0800

    Try to remove GMP

--- cmodel/rsa.cpp
+++ cmodel/rsa.cpp
@@ -1,5 +1,7 @@
 #include "verilog_int.h"
 #include "rsa.h"
+#include <iostream>
+using namespace std;
 
 void two_power_mod(mpz_t out, const unsigned power, const mpz_t N) {
   mpz_t base;
@@ -46,6 +48,19 @@ void montgomery_base2(mpz_t out, const mpz_t A, const mpz_t B, const mpz_t N) {
 }
 
 void montgomery_base2(rsa_key_t &out, const rsa_key_t &A, const rsa_key_t &B, const rsa_key_t &N) {
+  out = 0;
+  for (int i = 0; i < 256; ++i) {
+    if (A.Bit(i)) {
+      out += B;
+    }
+    if (out.Bit(0)) {
+      out += N;
+    }
+    out >>= 1;
+  }
+  if (out > N) {
+    out -= N;
+  }
 }
 
 void lsb_modular_exponentiation(mpz_t out, const mpz_t A, const mpz_t B, const mpz_t N) {
@@ -59,6 +74,7 @@ void lsb_modular_exponentiation(mpz_t out, const mpz_t A, const mpz_t B, const m
     if (bit_i) {
       montgomery_base2(multiple, multiple, square, N);
     }
+    // cout << mpz_get_str(nullptr, 16, square) << endl;
     montgomery_base2(square, square, square, N);
   }
 
@@ -66,6 +82,23 @@ void lsb_modular_exponentiation(mpz_t out, const mpz_t A, const mpz_t B, const m
 }
 
 void lsb_modular_exponentiation(rsa_key_t &out, const rsa_key_t &A, const rsa_key_t &B, const rsa_key_t &N) {
+  int src_idx = 0;
+  // use double buffer, each loop we use src_idx to compute dst_idx
+  rsa_key_t square[2], multiple[2];
+  multiple[src_idx] = 1;
+  square[src_idx] = A;
+  for (int i = 0; i < 256; ++i) {
+    const int dst_idx = 1 - src_idx;
+    if (B.Bit(i)) {
+      montgomery_base2(multiple[dst_idx], multiple[src_idx], square[src_idx], N);
+    } else {
+      multiple[dst_idx] = multiple[src_idx];
+    }
+    montgomery_base2(square[dst_idx], square[src_idx], square[src_idx], N);
+    // cout << square[src_idx] << endl;
+    src_idx = 1 - src_idx;
+  }
+  out = multiple[src_idx];
 }
 
 void rsa(mpz_t out, const mpz_t msg, const mpz_t key, const mpz_t N) {
--- cmodel/test_rsa.cpp
+++ cmodel/test_rsa.cpp
@@ -13,49 +13,74 @@ TEST(RsaTest, test_two_power_mod) {
   rsa_key_t in1, out, gold;
   from_hex(in1, str_in1);
   from_hex(gold, str_gold);
+
+  // run and check
   two_power_mod(out, 512, in1);
   EXPECT_EQ(out, gold);
 }
 
 TEST(RsaTest, test_montgomery_to_self) {
   // MA(A, 2 ** 256) = A
-  const char str_N[] = "0xE07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1";
-  const char str_A[] = "0x412820616369726641206874756F53202C48544542415A494C452054524F50";
+  string str_N("E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1");
+  string str_A("412820616369726641206874756F53202C48544542415A494C452054524F50");
+  rsa_key_t A, B, N, out;
+  from_hex(A, str_A);
+  from_hex(N, str_N);
 
-  mpz_t A, B, N, out;
-  mpz_init_set_str(A, str_A, 0);
-  mpz_init_set_str(N, str_N, 0);
-  mpz_inits(B, out, NULL);
-
+  // run and check
   two_power_mod(B, 256, N);
   montgomery_base2(out, A, B, N);
-
-  // check output
-  EXPECT_EQ(0, mpz_cmp(out, A));
+  EXPECT_EQ(out, A);
 }
 
-TEST(RsaTest, test_montgomery) {
-  // generate answer using RSA.py
+TEST(RsaTest, test_montgomery_orig) {
   const char str_N[] = "0xE07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1";
   const char str_A[] = "0x412820616369726641206874756f53202c48544542415a494c452054524f50";
   const char str_B[] = "0x10001";
   const char str_ans[] = "0x314f8acb18e57c4b2fa37adefa7964711b8dcdb7aac7514c78d97cf4d4121426";
-
   mpz_t A, B, N, out, ans;
   mpz_init_set_str(A, str_A, 0);
   mpz_init_set_str(B, str_B, 0);
   mpz_init_set_str(N, str_N, 0);
   mpz_init_set_str(ans, str_ans, 0);
   mpz_init(out);
-
   montgomery_base2(out, A, B, N);
-
-  // check output
   EXPECT_EQ(0, mpz_cmp(out, ans));
+
+  mpz_t sq, ans2;
+  const char str_sq[] = "0xbf5ecf932790b9c808118ad16e41045d881920f60b031743c84d51b9246778a4";
+  const char str_ans2[] = "0x6356aa97d0e04b2c4c9147e036a9439501f9a2c3e17c086be640372186ed94ae";
+  mpz_init_set_str(sq, str_sq, 0);
+  mpz_init_set_str(ans2, str_ans2, 0);
+  montgomery_base2(out, sq, sq, N);
+  EXPECT_EQ(0, mpz_cmp(out, ans2));
 }
 
-TEST(RsaTest, test_lsb) {
+TEST(RsaTest, test_montgomery) {
   // generate answer using RSA.py
+  string str_N("E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1");
+  string str_A("412820616369726641206874756f53202c48544542415a494c452054524f50");
+  string str_B("10001");
+  string str_ans("314f8acb18e57c4b2fa37adefa7964711b8dcdb7aac7514c78d97cf4d4121426");
+  rsa_key_t A, B, N, out, ans;
+  from_hex(A, str_A);
+  from_hex(B, str_B);
+  from_hex(N, str_N);
+  from_hex(ans, str_ans);
+  // run and check
+  montgomery_base2(out, A, B, N);
+  EXPECT_EQ(out, ans);
+
+  string str_sq("bf5ecf932790b9c808118ad16e41045d881920f60b031743c84d51b9246778a4");
+  string str_ans2("6356aa97d0e04b2c4c9147e036a9439501f9a2c3e17c086be640372186ed94ae");
+  from_hex(A, str_sq);
+  from_hex(ans, str_ans2);
+  // run and check
+  montgomery_base2(out, A, A, N);
+  EXPECT_EQ(out, ans);
+}
+
+TEST(RsaTest, test_lsb_orig) {
   const char str_N[] = "0xE07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1";
   const char str_A[] = "0x412820616369726641206874756f53202c48544542415a494c452054524f50";
   const char str_B[] = "0x10001";
@@ -69,9 +94,25 @@ TEST(RsaTest, test_lsb) {
   mpz_init(out);
 
   lsb_modular_exponentiation(out, A, B, N);
-
   // check output
   EXPECT_EQ(0, mpz_cmp(out, ans));
+}
+
+TEST(RsaTest, test_lsb) {
+  // generate answer using RSA.py
+  string str_N("E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1");
+  string str_A("412820616369726641206874756f53202c48544542415a494c452054524f50");
+  string str_B("10001");
+  string str_ans("C7F5B34038B0ED8E8B286C739B163BDDF7F4C60A7581A32F99C3F1FA99A474A5");
+  rsa_key_t A, B, N, out, ans;
+  from_hex(A, str_A);
+  from_hex(B, str_B);
+  from_hex(N, str_N);
+  from_hex(ans, str_ans);
+
+  // run and check
+  lsb_modular_exponentiation(out, A, B, N);
+  EXPECT_EQ(out, ans);
 }
 
 TEST(RsaTest, test_rsa) {
