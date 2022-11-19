1b345d5e7acc0c83bf5621d38026ec2824333747
Yoda Lee <lc85301@gmail.com>
2022-11-19 09:38:16 +0800

    Install glog, gflags, protobuf by apt

--- docker/install_verilator.sh
+++ docker/install_verilator.sh
@@ -1,25 +1,6 @@
 
 NPROC=4
 
-# install gflags
-wget https://github.com/gflags/gflags/archive/refs/tags/v2.2.2.tar.gz -O gflags.tar.gz && \
-tar zxf gflags.tar.gz && cd gflags-2.2.2 && \
-cmake -DCMAKE_INSTALL_PREFIX=${THE_PREFIX} -DBUILD_SHARED_LIBS=ON \
-	-DCMAKE_BUILD_TYPE=Release && ${SUDO} make -j${NPROC} install
-cd ..
-
-# install glog
-wget https://github.com/google/glog/archive/refs/tags/v0.6.0.tar.gz -O glog.tar.gz && \
-tar zxf glog.tar.gz && cd glog-0.6.0  && \
-cmake -DCMAKE_INSTALL_PREFIX=${THE_PREFIX} -DCMAKE_BUILD_TYPE=Release && ${SUDO} make -j${NPROC} install
-cd ..
-
-# install protobuf
-wget https://github.com/protocolbuffers/protobuf/archive/refs/tags/v21.9.tar.gz -O protobuf.tar.gz && \
-tar zxf protobuf.tar.gz && cd protobuf-21.9 && cd cmake && \
-cmake -DCMAKE_INSTALL_PREFIX=${THE_PREFIX} -DCMAKE_BUILD_TYPE=Release -Dprotobuf_BUILD_TESTS=OFF && ${SUDO} make -j${NPROC}
-cd ../..
-
 # install verilator
 wget https://github.com/verilator/verilator/archive/refs/tags/v4.228.tar.gz -O verilator.tar.gz && \
 tar zxf verilator.tar.gz && cd verilator-4.228 && \
