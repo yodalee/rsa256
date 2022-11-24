e6bfc118c27bf5275582efdd9905148ee5c247d3
Yoda Lee <lc85301@gmail.com>
2022-11-24 08:44:52 +0800

    Add scoreboard implementation

---
+++ common/include/scoreboard.h
@@ -0,0 +1,45 @@
+#include <functional>
+#include <glog/logging.h>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+template <typename DataType> class ScoreBoard {
+private:
+public:
+  ScoreBoard(::std::function<void()> RaiseFailure_)
+      : RaiseFailure(RaiseFailure_) {
+    assert(RaiseFailure);
+  };
+  ~ScoreBoard() = default;
+
+  void push_golden(const DataType &data) {
+    goldens.push_back(data);
+    check();
+  }
+  void push_received(const DataType &data) {
+    receiveds.push_back(data);
+    check();
+  }
+
+  void check() {
+    while (goldens.size() != 0 && receiveds.size() != 0) {
+      const DataType &received = receiveds.front();
+      const DataType &golden = goldens.front();
+
+      if (golden == received) {
+      } else {
+        LOG(ERROR) << "Golden != Verilog Out: " << golden << " vs " << received;
+        RaiseFailure();
+      }
+      goldens.pop_front();
+      receiveds.pop_front();
+    }
+  }
+
+private:
+  ::std::deque<DataType> goldens;
+  ::std::deque<DataType> receiveds;
+  ::std::function<void()> RaiseFailure;
+};
