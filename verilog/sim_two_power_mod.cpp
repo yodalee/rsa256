4c1c467bf5cee5223133eb3cfae6dbf110a65326
Yoda Lee <lc85301@gmail.com>
2022-12-12 23:52:02 +0800

    Migrate two_power_mod to test_fixture

--- verilog/sim_two_power_mod.cpp
+++ verilog/sim_two_power_mod.cpp
@@ -1,10 +1,8 @@
 
 #include "Vtwo_power_mod.h"
 #include "assign_port.h"
-#include "callback.h"
-#include "dut_wrapper.h"
 #include "model_rsa.h"
-#include "scoreboard.h"
+#include "test_fixture.h"
 #include "verilog_int.h"
 #include <iostream>
 #include <systemc>
@@ -12,69 +10,39 @@
 using namespace std;
 using namespace sc_core;
 
+using IN = RSATwoPowerModIn;
 using DUT = Vtwo_power_mod;
+using OUT = RSATwoPowerModOut;
 
-void KillSimulation() {
-  wait(100, SC_NS);
-  sc_stop();
-}
-
-SC_MODULE(Testbench) {
-  sc_clock clk;
-  DUTWrapper<DUT> dut_wrapper;
-  shared_ptr<Driver<RSATwoPowerModIn>> driver;
-  shared_ptr<Monitor<RSATwoPowerModOut>> monitor;
-  unique_ptr<ScoreBoard<RSATwoPowerModOut>> score_board;
-
+class TestBench_RsaTwoPowerMod : public TestBench<IN, OUT, DUT> {
 public:
-  SC_HAS_PROCESS(Testbench);
-  Testbench(const sc_module_name &name)
-      : sc_module(name), clk("clk", 1.0, SC_NS), dut_wrapper("dut_wrapper"),
-        score_board(new ScoreBoard<RSATwoPowerModOut>(KillSimulation)) {
-    dut_wrapper.clk(clk);
-    RSATwoPowerModOut golden;
-    from_hex(
-        golden,
-        "0AF39E1F831CB4FCD92B17F61F473735C687593A931C97D2B60AD6C7443F09FDB");
-    score_board->push_golden(golden);
+  using TestBench::TestBench;
 
-    driver = make_shared<Driver<RSATwoPowerModIn>>(
-        dut_wrapper.dut->i_valid, dut_wrapper.dut->i_ready,
-        [this](const RSATwoPowerModIn &in) { this->writer(in); });
-    monitor = make_shared<Monitor<RSATwoPowerModOut>>(
-        dut_wrapper.dut->o_valid, dut_wrapper.dut->o_ready,
-        [this]() { return this->reader(); },
-        [this](const RSATwoPowerModOut &out) { return this->notify(out); },
-        KillSimulation);
-    dut_wrapper.register_callback(driver);
-    dut_wrapper.register_callback(monitor);
+  virtual void writer(const InType &in) {
+    write_verilator_port(dut_wrapper.dut->i_power, in.power);
+    write_verilator_port(dut_wrapper.dut->i_modulus, in.modulus);
+  };
 
-    KeyType modulus;
-    from_hex(
-        modulus,
-        "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1");
-    driver->push_back({.power = verilog::vuint<32>(512), .modulus = modulus});
-  }
-
-  void notify(const RSATwoPowerModOut &out) { score_board->push_received(out); }
-
-  void writer(const RSATwoPowerModIn &in) {
-    auto power = in.power;
-    auto modulus = in.modulus;
-    std::cout << to_hex(modulus) << std::endl;
-    write_verilator_port(dut_wrapper.dut->i_power, power);
-    write_verilator_port(dut_wrapper.dut->i_modulus, modulus);
-  }
-
-  RSATwoPowerModOut reader() {
-    RSATwoPowerModOut out;
+  virtual OutType reader() {
+    OutType out;
     read_verilator_port(out, dut_wrapper.dut->o_out);
     return out;
-  }
+  };
 };
 
 int sc_main(int, char **) {
-  unique_ptr<Testbench> testbench(new Testbench("testbench"));
+  unique_ptr<TestBench_RsaTwoPowerMod> testbench(
+      new TestBench_RsaTwoPowerMod("TestBench_two_power_mod_sv"));
+  KeyType modulus;
+  from_hex(modulus,
+           "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1");
+  testbench->push_input({.power = verilog::vuint<32>(512), .modulus = modulus});
+
+  TestBench_RsaTwoPowerMod::OutType golden;
+  from_hex(golden,
+           "0AF39E1F831CB4FCD92B17F61F473735C687593A931C97D2B60AD6C7443F09FDB");
+  testbench->push_golden(golden);
+
   sc_start(1200.0, SC_NS);
   return 0;
 }
