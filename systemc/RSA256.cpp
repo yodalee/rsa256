a639da4b7cc194890b0eb971f386f26aece4c986
Yoda Lee <lc85301@gmail.com>
2022-12-23 23:33:42 +0800

    RSA256 using one montgomery module

--- systemc/RSA256.cpp
+++ systemc/RSA256.cpp
@@ -10,10 +10,28 @@ using namespace verilog;
 void RSA256::Thread() {
   char str[256];
   while (true) {
-    RSAModIn in = i_data.read();
-    vuint<kBW> crypto;
-    rsa(crypto, in.msg, in.key, in.modulus);
-    cout << crypto << endl;
+    const RSAModIn &in = i_data.read();
+    const RSATwoPowerModIn::TwoPowerMod_Power_t vuint512{512};
+    two_power_mod_in.write({.power = vuint512, .modulus = in.modulus});
+
+    auto pack_value = two_power_mod_out.read();
+    montgomery_in.write({.a = in.msg, .b = pack_value, .modulus = in.modulus});
+    auto packed_msg = montgomery_out.read();
+    const auto &key = in.key;
+
+    KeyType crypto;
+    KeyType multiple{1};
+    KeyType square{packed_msg};
+    for (size_t i = 0; i < kBW; i++) {
+      if (key.Bit(i)) {
+        montgomery_in.write(
+            {.a = multiple, .b = square, .modulus = in.modulus});
+        multiple = montgomery_out.read();
+      }
+      montgomery_in.write({.a = square, .b = square, .modulus = in.modulus});
+      square = montgomery_out.read();
+    }
+    crypto = multiple;
     o_crypto.write(crypto);
   }
 }
--- systemc/RSA256.h
+++ systemc/RSA256.h
@@ -1,3 +1,5 @@
+#include "RSAMontgomery.h"
+#include "RSATwoPowerMod.h"
 #include "model_rsa.h"
 #include "verilog_int.h"
 #include <systemc>
@@ -12,7 +14,23 @@ SC_MODULE(RSA256) {
   sc_fifo_in<RSAModIn> i_data;
   sc_fifo_out<KeyType> o_crypto;
 
-  SC_CTOR(RSA256) { SC_THREAD(Thread); }
+  RSAMontgomery i_montgomery;
+  RSATwoPowerMod i_two_power_mod;
+  sc_fifo<RSATwoPowerModIn> two_power_mod_in;
+  sc_fifo<RSATwoPowerModOut> two_power_mod_out;
+  sc_fifo<RSAMontgomeryModIn> montgomery_in;
+  sc_fifo<RSAMontgomeryModOut> montgomery_out;
+
+  SC_CTOR(RSA256)
+      : i_montgomery("i_montgomery"), i_two_power_mod("i_two_power_mod") {
+    i_two_power_mod.clk(clk);
+    i_two_power_mod.data_in(two_power_mod_in);
+    i_two_power_mod.data_out(two_power_mod_out);
+    i_montgomery.clk(clk);
+    i_montgomery.data_in(montgomery_in);
+    i_montgomery.data_out(montgomery_out);
+    SC_THREAD(Thread);
+  }
 
   void Thread();
 };
