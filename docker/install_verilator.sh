dfc98734051b4ddab8d93df37c2793be6dfc90d4
yodalee <lc85301@gmail.com>
2023-02-03 18:06:55 +0800

    update action to auto-build docker image

--- docker/install_verilator.sh
+++ docker/install_verilator.sh
@@ -5,3 +5,4 @@ NPROC=4
 wget https://github.com/verilator/verilator/archive/refs/tags/v4.228.tar.gz -O verilator.tar.gz && \
 tar zxf verilator.tar.gz && cd verilator-4.228 && \
 autoconf && ./configure --prefix=${THE_PREFIX} && make -j${NPROC} && ${SUDO} make install
+exit 0
