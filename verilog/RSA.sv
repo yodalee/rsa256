7bddf2a130573ad0b505667aec423ace51e099c3
Yoda Lee <lc85301@gmail.com>
2023-02-04 22:32:11 +0800

    Instantiate montgomery and 2^ mod in RSA

--- verilog/RSA.sv
+++ verilog/RSA.sv
@@ -20,4 +20,56 @@ module RSA (
 	output KeyType o_crypto
 );
 
+logic two_power_mod_in_valid;
+logic two_power_mod_in_ready;
+KeyType two_power_mod_in_modulus;
+IntType two_power_mod_in_power;
+logic two_power_mod_out_valid;
+logic two_power_mod_out_ready;
+KeyType two_power_mod_out;
+
+RSATwoPowerMod i_two_power_mod (
+	// input
+	.clk(clk),
+	.rst(rst),
+
+	// input data
+	.i_valid(two_power_mod_in_valid),
+	.i_ready(two_power_mod_in_ready),
+	.i_modulus(two_power_mod_in_modulus),
+	.i_power(two_power_mod_in_power),
+
+	// output data
+	.o_valid(two_power_mod_out_valid),
+	.o_ready(two_power_mod_out_ready),
+	.o_out(two_power_mod_out)
+);
+
+logic montgomery_in_valid;
+logic montgomery_in_ready;
+KeyType montgomery_in_a;
+KeyType montgomery_in_b;
+KeyType montgomery_in_modulus;
+logic montgomery_out_valid;
+logic montgomery_out_ready;
+KeyType montgomery_out;
+
+RSAMontgomery i_montgomery (
+	// input
+	.clk(clk),
+	.rst(rst),
+
+	// input data
+	.i_valid(montgomery_in_valid),
+	.i_ready(montgomery_in_ready),
+	.i_a(montgomery_in_a),
+	.i_b(montgomery_in_b),
+	.i_modulus(montgomery_in_modulus),
+
+	// output data
+	.o_valid(montgomery_out_valid),
+	.o_ready(montgomery_out_ready),
+	.o_out(montgomery_out)
+);
+
 endmodule
--- verilog/RSA_pkg.sv
+++ verilog/RSA_pkg.sv
@@ -4,4 +4,6 @@ parameter MOD_WIDTH = 256;
 
 typedef logic [MOD_WIDTH-1:0] KeyType;
 
+typedef logic [31:0] IntType;
+
 endpackage
