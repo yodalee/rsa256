9c044441b6efe91faedd87a23a5f672c47f97ae4
yodalee <lc85301@gmail.com>
2023-02-04 22:32:11 +0800

    Add one test data in RSA256

--- verilog/RSA256_sim.cpp
+++ verilog/RSA256_sim.cpp
@@ -31,8 +31,26 @@ public:
   }
 };
 
+const char str_msg[] =
+    "412820616369726641206874756F53202C48544542415A494C452054524F50";
+const char str_key[] = "10001";
+const char str_modulus[] =
+    "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1";
+const char str_ans[] =
+    "0D41B183313D306ADCA09126F3FED6CDEC7DCDCE49DB5C85CB2A37F08C0F2E31";
+
 int sc_main(int, char **) {
   unique_ptr<TestBench_Rsa> testbench(new TestBench_Rsa("testbench_rsa_sv"));
-  testbench->run(1200, SC_NS);
+  // sample in
+  RSAModIn in;
+  from_hex(in.msg, str_msg);
+  from_hex(in.key, str_key);
+  from_hex(in.modulus, str_modulus);
+  testbench->push_input(in);
+  // sample out
+  RSAModOut ans;
+  from_hex(ans, str_ans);
+  testbench->push_golden(ans);
+  testbench->run(160 * 1000, SC_NS);
   return 0;
 }
