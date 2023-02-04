8398928ae4c013433f0cde7fbb529330875d13c7
Yoda Lee <yodalee@users.noreply.github.com>
2023-02-04 10:22:49 +0800

    update action to auto-build docker image (#58)
    * update action to auto-build and push docker image
    * bump verilator from 4.228 to 5.006

--- docker/install_verilator.sh
+++ docker/install_verilator.sh
@@ -2,6 +2,8 @@
 NPROC=4
 
 # install verilator
-wget https://github.com/verilator/verilator/archive/refs/tags/v4.228.tar.gz -O verilator.tar.gz && \
-tar zxf verilator.tar.gz && cd verilator-4.228 && \
+
+wget https://github.com/verilator/verilator/archive/refs/tags/v5.006.tar.gz -O verilator.tar.gz && \
+tar zxf verilator.tar.gz && cd verilator-5.006 && \
 autoconf && ./configure --prefix=${THE_PREFIX} && make -j${NPROC} && ${SUDO} make install
+exit 0
