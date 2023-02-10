aefc20a90401a068126d2842bfdba254b8623c4b
Yoda Lee <lc85301@gmail.com>
2023-02-11 00:20:00 +0800

    Add RSA256 testdata from dclab

--- cmodel/test_rsa.cpp
+++ cmodel/test_rsa.cpp
@@ -106,6 +106,76 @@ TEST(RsaTest, test_rsa) {
   EXPECT_EQ(out, c0);
 }
 
+struct RsaData {
+  string N;
+  string key;
+  string m;
+  string c;
+};
+
+class RsaParameterSuite : public ::testing::TestWithParam<RsaData> {};
+
+TEST_P(RsaParameterSuite, RsaParameterTest) {
+  auto param = GetParam();
+  rsa_key_t N, key, m, c, out;
+  from_hex(N, param.N);
+  from_hex(key, param.key);
+  from_hex(m, param.m);
+  from_hex(c, param.c);
+
+  // check output
+  rsa(out, m, key, N);
+  EXPECT_EQ(out, c);
+}
+
+const vector<RsaData> dclabdataset{
+    RsaData{
+        .N = "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1",
+        .key = "10001",
+        // P)-Don't call the Dutch underac
+        .m = "63617265646E7520686374754420656874206C6C61632074276E6F442D2950",
+        .c = "2EE3A71DAA16525340F365965046E0D01E348E4619ABFDBC81EA4E4799039ADF",
+    },
+    RsaData{
+        .N = "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1",
+        .key = "10001",
+        // hievers anymore. Not after the
+        .m = "2065687420726574666120746F4E202E65726F6D796E612073726576656968",
+        .c = "4E7EC5ACEBEC5CE50CB46DF13D251FA652CB89E9157B1F20C062CD4ECD20D801",
+    },
+    RsaData{
+        .N = "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1",
+        .key = "10001",
+        // way the Netherlands rallied to
+        .m = "206F74206465696C6C61722073646E616C72656874654E2065687420796177",
+        .c =
+            "5A45C7C2D977C762F525888C55A025E5F621E11BDFBF82F91B3D9E46EE2D3931"},
+    RsaData{
+        .N = "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1",
+        .key = "10001",
+        // upset five-time champion Brazil
+        .m = "6C697A617242206E6F69706D61686320656D69742D65766966207465737075",
+        .c =
+            "C60BFA4C927DDF1A4F1E55C3AD10E65858AC609E668C2092E17D90D6AF29B71A"},
+    RsaData{
+        .N = "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1",
+        .key = "10001",
+        // 2-1 in the World Cup quarterfi
+        .m = "6966726574726175712070754320646C726F5720656874206E6920312D3220",
+        .c = "18C9E09D871B4B1EF5DD844C2BF9B849A70E785652786421E24939C8EDADFDEE",
+    },
+    RsaData{
+        .N = "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1",
+        .key = "10001",
+        // nals Friday. After waking thems
+        .m = "736D65687420676E696B6177207265746641202E79616469724620736C616E",
+        .c = "39D7C84C563C9A593A66291C72A0F7FBBEF7B3F21DFEDE437325229EEF07F6B6",
+    },
+};
+
+INSTANTIATE_TEST_SUITE_P(DcLabSet, RsaParameterSuite,
+                         ::testing::ValuesIn(dclabdataset));
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
