1eb4fef254140fc38cb58c32e6c65d19445e51c2
Yu-Sheng Lin <johnjohnlys@media.ee.ntu.edu.tw>
2023-02-19 12:12:12 +0800

    Update SystemC in docker to C++17 (#63)

--- docker/install_systemc.sh
+++ docker/install_systemc.sh
@@ -4,6 +4,6 @@ NPROC=4
 # install systemc
 wget https://github.com/accellera-official/systemc/archive/refs/tags/2.3.4.tar.gz -O systemc.tar.gz && \
 tar zxf systemc.tar.gz && cd systemc-2.3.4 && \
-cmake -DCMAKE_INSTALL_PREFIX=${THE_PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=14 && \
+cmake -DCMAKE_INSTALL_PREFIX=${THE_PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 && \
 	make -j${NPROC} install
 exit 0
