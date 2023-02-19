81e33d9765a5afa53079d0c5b33d9de5f2657885
Yoda Lee <yodalee@users.noreply.github.com>
2023-02-19 09:47:00 +0800

    Build systemc instead of ubuntu apt (#62)
    Looks good

---
+++ docker/install_systemc.sh
@@ -0,0 +1,9 @@
+
+NPROC=4
+
+# install systemc
+wget https://github.com/accellera-official/systemc/archive/refs/tags/2.3.4.tar.gz -O systemc.tar.gz && \
+tar zxf systemc.tar.gz && cd systemc-2.3.4 && \
+cmake -DCMAKE_INSTALL_PREFIX=${THE_PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=14 && \
+	make -j${NPROC} install
+exit 0
