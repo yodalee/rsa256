b030a522be8e08b76a3aa4a09fc88cde088eeb30
Yoda Lee <lc85301@gmail.com>
2023-03-10 17:21:37 +0800

    Construct data from hex string

--- verilog/RSATwoPowerMod_sim.cpp
+++ verilog/RSATwoPowerMod_sim.cpp
@@ -1,8 +1,8 @@
 
 #include "VRSATwoPowerMod.h"
+#include "bridge/verilator/testbench.h"
 #include "bridge/verilator/verilator_assign.h"
 #include "model_rsa.h"
-#include "bridge/verilator/testbench.h"
 #include "verilog/dtype.h"
 #include <iostream>
 #include <systemc>
@@ -33,15 +33,13 @@ public:
 int sc_main(int, char **) {
   unique_ptr<TestBench_RsaTwoPowerMod> testbench(new TestBench_RsaTwoPowerMod(
       "TestBench_two_power_mod_sv", /*dump=*/true));
-  KeyType modulus;
-  from_hex(modulus,
-           "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1");
+  KeyType modulus(
+      "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1");
   testbench->push_input(
       {.power = RSATwoPowerModIn::IntType(512), .modulus = modulus});
 
-  TestBench_RsaTwoPowerMod::OutType golden;
-  from_hex(golden,
-           "0AF39E1F831CB4FCD92B17F61F473735C687593A931C97D2B60AD6C7443F09FDB");
+  TestBench_RsaTwoPowerMod::OutType golden(
+      "0AF39E1F831CB4FCD92B17F61F473735C687593A931C97D2B60AD6C7443F09FDB");
   testbench->push_golden(golden);
 
   return testbench->run(1200, SC_NS);
