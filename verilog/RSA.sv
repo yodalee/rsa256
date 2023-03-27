efb70500a45f86ab06dbaa00b1fee9a77f51bfd4
Yoda Lee <lc85301@gmail.com>
2023-03-27 23:10:33 +0800

    Migrate RSA to pipeline

--- verilog/RSA.sv
+++ verilog/RSA.sv
@@ -17,73 +17,25 @@ module RSA (
   output RSAModOut o_out
 );
 
-typedef enum logic {
-  STATE_IDLE = 0,
-  STATE_WAITDONE = 1
-} State_t;
-State_t state, state_next;
-
 KeyType msg, key, modulus;
 
-assign i_ready = state == STATE_IDLE;
-
-// update state
-always_ff @(posedge clk or negedge rst) begin
-  if (!rst) begin
-    state <= STATE_IDLE;
-  end
-  else begin
-    case (state)
-    STATE_IDLE: begin
-      if (i_valid) begin
-        state <= STATE_WAITDONE;
-        // kick start the two_power_mod
-        two_power_mod_in_valid <= 1'b1;
-      end
-    end
-    STATE_WAITDONE: begin
-      two_power_mod_in_valid <= 1'b0;
-      if (o_ready && o_valid) begin
-        state <= STATE_IDLE;
-      end
-    end
-    endcase
-  end
-end
-
 // read input data
-always_ff @( posedge clk or negedge rst ) begin
+always_ff @( posedge clk or negedge rst) begin
   if (!rst) begin
     msg <= 0;
     key <= 0;
     modulus <= 0;
-    two_power_mod_in_valid <= 0;
-  end
-  else if (state == STATE_IDLE && i_valid) begin
-    msg <= i_in.msg;
-    key <= i_in.key;
-    modulus <= i_in.modulus;
-    // kick start the two_power_mod
-    two_power_mod_in_valid <= 1'b1;
-  end
-  else if (state == STATE_WAITDONE) begin
-    msg <= msg;
-    key <= key;
-    modulus <= modulus;
-    two_power_mod_in_valid <= 1'b0;
   end
   else begin
-    msg <= msg;
-    key <= key;
-    modulus <= modulus;
-    two_power_mod_in_valid <= two_power_mod_in_valid;
+    if (i_valid && i_ready) begin
+      msg <= i_in.msg;
+      key <= i_in.key;
+      modulus <= i_in.modulus;
+    end
   end
 end
 
-logic two_power_mod_in_valid;
-logic two_power_mod_in_ready;
 KeyType packed_val;
-
 logic packed_valid;
 logic packed_ready;
 IntType power;
@@ -95,9 +47,9 @@ RSATwoPowerMod i_two_power_mod (
   .rst(rst),
 
   // input data
-  .i_valid(two_power_mod_in_valid),
-  .i_ready(two_power_mod_in_ready),
-  .i_in({power, modulus}),
+  .i_valid(i_valid),
+  .i_ready(i_ready),
+  .i_in({power, i_in.modulus}),
 
   // output data
   .o_valid(packed_valid),
