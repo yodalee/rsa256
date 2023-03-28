2300e49aa5cca33354fdb4684de628b3886b0507
Yoda Lee <lc85301@gmail.com>
2023-03-29 01:05:51 +0800

    Refactor Montgomery using Pipeline

--- verilog/RSAMontgomery.sv
+++ verilog/RSAMontgomery.sv
@@ -20,12 +20,6 @@ module RSAMontgomery #(
 
 typedef logic [MOD_WIDTH + 2 - 1:0] ExtendKeyType;
 
-typedef enum logic [1:0] {
-  STATE_IDLE  = 0,
-  STATE_CALCULATE = 1,
-  STATE_WAITDONE = 2
-} State_t;
-
 ExtendKeyType data_a;
 ExtendKeyType data_b;
 ExtendKeyType data_modulus;
@@ -35,46 +29,8 @@ ExtendKeyType round_result_next;
 
 logic [$clog2(MOD_WIDTH+1)-1:0] round_counter;
 
-State_t state, state_next;
-
-assign i_ready = state == STATE_IDLE;
-assign o_valid = state == STATE_WAITDONE;
 assign mod_result = round_result > data_modulus ? round_result - data_modulus : round_result;
 assign o_out = mod_result[MOD_WIDTH - 1 : 0];
-
-// update state logic
-always_ff @(posedge clk or negedge rst) begin
-  if (!rst) begin
-    state <= STATE_IDLE;
-  end
-  else begin
-    state <= state_next;
-  end
-end
-
-// next logic for state
-always_comb begin
-  case (state)
-  STATE_IDLE: begin
-    if (i_valid) begin
-      state_next = STATE_CALCULATE;
-    end
-  end
-  STATE_CALCULATE: begin
-    if (round_counter == MOD_WIDTH-1) begin
-      state_next = STATE_WAITDONE;
-    end
-  end
-  STATE_WAITDONE: begin
-    if (o_ready) begin
-      state_next = STATE_IDLE;
-    end
-  end
-  default: begin
-    state_next = STATE_IDLE;
-  end
-  endcase
-end
 
 // read input data
 always_ff @( posedge clk or negedge rst ) begin
@@ -88,26 +44,17 @@ always_ff @( posedge clk or negedge rst ) begin
       data_a <= {2'b0, i_in.a};
       data_b <= {2'b0, i_in.b};
       data_modulus <= {2'b0, i_in.modulus};
-    end else begin
-      data_a <= data_a;
-      data_b <= data_b;
-      data_modulus <= data_modulus;
     end
   end
 end
 
 // round_counter
 always_ff @(posedge clk or negedge rst) begin
-  if (!rst) begin
+  if (loop_init) begin
     round_counter <= 0;
   end
-  else begin
-  case (state)
-    STATE_CALCULATE:
-      round_counter <= round_counter + 1;
-    default:
-    round_counter <= 0;
-  endcase
+  else if (loop_next) begin
+    round_counter <= round_counter + 1;
   end
 end
 
@@ -115,33 +62,53 @@ end
 always_ff @(posedge clk or negedge rst) begin
   if (!rst) begin
     round_result <= 0;
-  end
-  else begin
-    round_result <= round_result_next;
+  end else begin
+    if (loop_init) begin
+      round_result <= 0;
+    end
+    else if (loop_done) begin
+      round_result <= round_result;
+    end
+    else begin
+      round_result <= round_result_next;
+    end
   end
 end
 
 always_comb begin
-  case (state)
-  STATE_IDLE: begin
-    round_result_next = 0;
+  round_result_next = round_result;
+  if (data_a[round_counter]) begin
+    round_result_next = round_result + data_b;
   end
-  STATE_CALCULATE: begin
-    if (data_a[round_counter]) begin
-      round_result_next = round_result + data_b;
-    end
-    if (round_result_next[0]) begin
-      round_result_next += data_modulus;
-    end
-    round_result_next >>= 1;
+  if (round_result_next[0]) begin
+    round_result_next += data_modulus;
   end
-  STATE_WAITDONE: begin
-    round_result_next = round_result;
-  end
-  default: begin
-    round_result_next = 0;
-  end
-  endcase
+  round_result_next >>= 1;
 end
+
+logic loop_ovalid, loop_oready;
+logic loop_init, loop_next;
+
+logic loop_done = round_counter == MOD_WIDTH;
+
+PipelineLoop i_loop(
+  .clk(clk),
+  .rst(rst),
+  .i_valid(i_valid),
+  .i_ready(i_ready),
+  .i_cen(loop_init),
+  .o_valid(loop_ovalid),
+  .o_ready(loop_oready),
+  .o_done(loop_done),
+  .o_cen(loop_next)
+);
+
+PipelineFilter i_filter(
+    .i_valid(loop_ovalid),
+    .i_ready(loop_oready),
+    .i_pass(loop_done),
+    .o_valid(o_valid),
+    .o_ready(o_ready)
+);
 
 endmodule
