b280a0bff50644d09e42cfd86ab91a7ba9ef7472
Yoda Lee <yodalee@users.noreply.github.com>
2023-03-31 01:05:10 +0800

    Refactor RSAMont using Pipeline (#107)

--- verilog/RSAMont.sv
+++ verilog/RSAMont.sv
@@ -20,21 +20,15 @@ module RSAMont (
   output KeyType o_crypto
 );
 
-typedef enum logic [1:0] {
-  STATE_IDLE = 0,
-  STATE_LOOP = 1,
-  STATE_WAITDONE = 2
-} State_t;
-State_t state, state_next;
+localparam N_FANOUT = 2;
 
 KeyType base, msg, key, modulus; // input
 KeyType square, multiply;  // calculate result
+
 logic [$clog2(2 * MOD_WIDTH+1)-1:0] round_counter;
 logic [$clog2(2 * MOD_WIDTH+1)-1:0] key_idx;
-
-assign i_ready = state == STATE_IDLE;
-assign o_valid = state == STATE_WAITDONE;
 assign key_idx = (round_counter - 2) >> 1;
+assign o_crypto = multiply;
 
 // read input data
 always_ff @( posedge clk or negedge rst ) begin
@@ -44,62 +38,17 @@ always_ff @( posedge clk or negedge rst ) begin
     key <= 0;
     modulus <= 0;
   end
-  else begin
-    if (i_ready && i_valid) begin
-      base <= i_base;
-      msg <= i_msg;
-      key <= i_key;
-      modulus <= i_modulus;
-    end
-    else begin
-      base <= base;
-      msg <= msg;
-      key <= key;
-      modulus <= modulus;
-    end
+  else if (i_ready && i_valid) begin
+    base <= i_base;
+    msg <= i_msg;
+    key <= i_key;
+    modulus <= i_modulus;
   end
-end
-
-// update state logic
-always_ff @(posedge clk or negedge rst) begin
-  if (!rst) begin
-    state <= STATE_IDLE;
-  end
-  else begin
-    state <= state_next;
-  end
-end
-
-// next logic for state
-always_comb begin
-  case (state)
-  STATE_IDLE: begin
-    if (i_valid && i_ready) begin
-      state_next = STATE_LOOP;
-    end
-  end
-  STATE_LOOP: begin
-    if (loop_done) begin
-      state_next = STATE_WAITDONE;
-    end
-  end
-  STATE_WAITDONE: begin
-    if (o_ready) begin
-      state_next = STATE_IDLE;
-    end
-  end
-  default: begin
-    state_next = STATE_IDLE;
-  end
-  endcase
 end
 
 // round_counter
 always_ff @(posedge clk or negedge rst) begin
-  if (state != STATE_LOOP) begin
-    round_counter <= 0;
-  end
-  else if (loop_init) begin
+  if (loop_init) begin
     round_counter <= 0;
   end
   else if (loop_next) begin
@@ -109,7 +58,7 @@ end
 
 // data
 always_ff @(posedge clk) begin
-  if (montgomery_out_ready && montgomery_out_valid) begin
+  if (montgomery_o_valid) begin
     if (round_counter == 1) begin
       square <= montgomery_out;
       multiply <= 1;
@@ -123,59 +72,53 @@ always_ff @(posedge clk) begin
       multiply <= multiply;
     end
   end
-  else begin
-    square <= square;
-    multiply <= multiply;
-  end
 end
 
 always_comb begin
-  if (state == STATE_LOOP) begin
-    if (round_counter == 0) begin
-      montgomery_in_a = base;
-      montgomery_in_b = msg;
-    end
-    else if (round_counter[0] == 1'b1) begin
-      montgomery_in_a = square;
-      montgomery_in_b = multiply;
-    end
-    else begin
-      montgomery_in_a = square;
-      montgomery_in_b = square;
-    end
-  end 
+  if (round_counter == 0) begin
+    montgomery_in_a = base;
+    montgomery_in_b = msg;
+  end
+  else if (round_counter[0] == 1'b1) begin
+    montgomery_in_a = square;
+    montgomery_in_b = multiply;
+  end
   else begin
-    montgomery_in_a = 0;
-    montgomery_in_b = 0;
+    montgomery_in_a = square;
+    montgomery_in_b = square;
   end
 end
 
-logic montgomery_out_valid;
-logic montgomery_out_ready;
-KeyType montgomery_out;
-KeyType montgomery_in_a, montgomery_in_b;
+logic montgomery_o_valid;
+KeyType montgomery_in_a, montgomery_in_b, montgomery_out;
 
-logic loop_ivalid, loop_iready;
-logic loop_ovalid, loop_oready;
+logic [N_FANOUT-1:0] dist_o_valid, dist_o_ready;
+logic loop_i_valid, loop_i_ready, loop_o_valid, loop_o_ready;
 logic loop_init, loop_next, loop_done;
-assign loop_ivalid = state == STATE_LOOP;
 // check stop condition:
 // 1 round for packed message
-// 256 * 2 round for square and multiply
-assign loop_done = state == STATE_LOOP && round_counter == MOD_WIDTH * 2 + 2;
-assign montgomery_out_ready = state == STATE_LOOP;
-assign o_crypto = multiply;
+// 256 * 2 round: odd round for multiply, even round for square
+assign loop_done = round_counter == MOD_WIDTH * 2 + 2;
 
 PipelineLoop i_loop(
   .clk(clk),
   .rst(rst),
-  .i_valid(loop_ivalid),
-  .i_ready(loop_iready),
+  .i_valid(i_valid),
+  .i_ready(i_ready),
   .i_cen(loop_init),
-  .o_valid(loop_ovalid),
-  .o_ready(loop_oready),
+  .o_valid(loop_o_valid),
+  .o_ready(loop_o_ready),
   .o_done(loop_done),
   .o_cen(loop_next)
+);
+
+PipelineDistribute #(.N(N_FANOUT)) i_dist (
+  .clk(clk),
+  .rst(rst),
+  .i_valid(loop_o_valid),
+  .i_ready(loop_o_ready),
+  .o_valid(dist_o_valid),
+  .o_ready(dist_o_ready)
 );
 
 RSAMontgomery i_montgomery(
@@ -184,14 +127,22 @@ RSAMontgomery i_montgomery(
   .rst(rst),
 
   // input data
-  .i_valid(loop_ovalid),
-  .i_ready(loop_oready),
+  .i_valid(dist_o_valid[0]),
+  .i_ready(dist_o_ready[0]),
   .i_in({montgomery_in_a, montgomery_in_b, modulus}),
 
   // output data
-  .o_valid(montgomery_out_valid),
-  .o_ready(montgomery_out_ready),
+  .o_valid(montgomery_o_valid),
+  .o_ready(1'b1), // always ready
   .o_out(montgomery_out)
+);
+
+PipelineFilter i_filter(
+    .i_valid(dist_o_valid[1]),
+    .i_ready(dist_o_ready[1]),
+    .i_pass(loop_done),
+    .o_valid(o_valid),
+    .o_ready(o_ready)
 );
 
 endmodule
