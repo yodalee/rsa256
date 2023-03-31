cd32623d7f96433f7685ddc153604b9ba7ac26d1
Yoda Lee <lc85301@gmail.com>
2023-04-01 00:22:02 +0800

    Add header for RSAMont

--- include/model_rsa.h
+++ include/model_rsa.h
@@ -8,15 +8,6 @@
 constexpr int kBW = 256;
 using KeyType = verilog::vuint<kBW>;
 
-struct RSAModIn {
-  KeyType msg;
-  KeyType key;
-  KeyType modulus;
-  MAKE_VSTRUCT(msg, key, modulus)
-  VSTRUCT_HAS_PROCESS(RSAModIn)
-};
-using RSAModOut = KeyType;
-
 struct RSATwoPowerModIn {
   using IntType = verilog::vuint<32>;
   IntType power;
@@ -34,3 +25,22 @@ struct RSAMontgomeryModIn {
   VSTRUCT_HAS_PROCESS(RSAMontgomeryModIn)
 };
 using RSAMontgomeryModOut = KeyType;
+
+struct RSAMontModIn {
+  KeyType base; // 2 ^ 2n mod N
+  KeyType msg;
+  KeyType key;
+  KeyType modulus;
+  MAKE_VSTRUCT(base, msg, key, modulus)
+  VSTRUCT_HAS_PROCESS(RSAMontModIn)
+};
+using RSAMontModOut = KeyType;
+
+struct RSAModIn {
+  KeyType msg;
+  KeyType key;
+  KeyType modulus;
+  MAKE_VSTRUCT(msg, key, modulus)
+  VSTRUCT_HAS_PROCESS(RSAModIn)
+};
+using RSAModOut = KeyType;
