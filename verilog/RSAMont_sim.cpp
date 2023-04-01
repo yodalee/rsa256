afb6d3c193e3f3ea875626b58cfc3672d567ad0a
Yoda Lee <lc85301@gmail.com>
2023-04-01 17:43:51 +0800

    Add verilog test for RSAMont

---
+++ verilog/RSAMont_sim.cpp
@@ -0,0 +1,79 @@
+
+#include "VRSAMont.h"
+#include "bridge/verilator/testbench.h"
+#include "bridge/verilator/verilator_assign.h"
+#include "model_rsa.h"
+#include "source.h"
+#include "verilog/dtype.h"
+#include <iostream>
+#include <systemc>
+
+using namespace std;
+using namespace sc_core;
+using namespace verilog::verilator;
+
+using IN = RSAMontModIn;
+using DUT = VRSAMont;
+using OUT = RSAMontModOut;
+
+class TestBench_RsaMont : public TestBench<IN, OUT, DUT> {
+public:
+  using TestBench::TestBench;
+};
+
+class Driver : public InputConnector<IN, DUT> {
+public:
+  using InputConnector::InputConnector;
+  void write(DUT *dut, const IN &in) override {
+    write_port(dut->i_base, in.base);
+    write_port(dut->i_msg, in.msg);
+    write_port(dut->i_key, in.key);
+    write_port(dut->i_modulus, in.modulus);
+  }
+};
+
+class Monitor : public OutputConnector<OUT, DUT> {
+public:
+  using OutputConnector::OutputConnector;
+  OUT read(const DUT *dut) override {
+    OUT out;
+    read_port(out, dut->o_crypto);
+    return out;
+  }
+};
+
+int sc_main(int, char **) {
+  unique_ptr<TestBench_RsaMont> testbench(
+      new TestBench_RsaMont("testbench_rsamont_sv", /*dump=*/true));
+
+  auto driver =
+      make_shared<Driver>(testbench->dut_wrapper.dut->i_valid,
+                          testbench->dut_wrapper.dut->i_ready, nullptr);
+  auto monitor = make_shared<Monitor>(
+      testbench->dut_wrapper.dut->o_valid, testbench->dut_wrapper.dut->o_ready,
+      [&](const OUT &out) { return testbench->notify(out); }, KillSimulation,
+      nullptr);
+  testbench->register_connector(
+      static_cast<shared_ptr<Connector<DUT>>>(driver));
+  testbench->register_connector(
+      static_cast<shared_ptr<Connector<DUT>>>(monitor));
+
+  // sample in
+  RSAMontModIn in;
+  from_hex(in.base,
+           "0AF39E1F831CB4FCD92B17F61F473735C687593A931C97D2B60AD6C7443F09FDB");
+  from_hex(in.msg,
+           "412820616369726641206874756F53202C48544542415A494C452054524F50");
+  from_hex(in.key, "10001");
+  from_hex(in.modulus,
+           "E07122F2A4A9E81141ADE518A2CD7574DCB67060B005E24665EF532E0CCA73E1");
+
+  driver->push_back(in);
+
+  // sample out
+  RSAMontModOut out;
+  from_hex(out,
+           "0D41B183313D306ADCA09126F3FED6CDEC7DCDCE49DB5C85CB2A37F08C0F2E31");
+  testbench->push_golden(out);
+  return testbench->run(400, SC_US);
+}
