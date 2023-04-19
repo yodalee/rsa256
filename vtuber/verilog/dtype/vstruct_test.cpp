864cde23b95fb82b93943cfad88355038e783eed
Yoda Lee <yodalee@users.noreply.github.com>
2023-04-20 00:42:36 +0800

    Unable to assign value to array in vstruct (#129)
    * add failed test of assign value to array in struct
    * always return by reference in subscript operator
    Co-authored-by: Yu-Sheng Lin <johnjohnlys@gmail.com>

--- vtuber/verilog/dtype/vstruct_test.cpp
+++ vtuber/verilog/dtype/vstruct_test.cpp
@@ -37,3 +37,28 @@ TEST(TestVerilogStruct, Pack) {
 	vuint<21> tmp = pack(s1);
 	EXPECT_EQ(tmp, 06543210);
 }
+
+typedef varray<vuint<64>, 5> Arr3;
+typedef vuint<8> C;
+
+struct S2 {
+	Arr3 arr;
+	C c;
+	MAKE_VSTRUCT(arr, c)
+	VSTRUCT_HAS_PROCESS(S2)
+};
+
+TEST(TestVerilogStruct, AssignValue) {
+	S2 s2;
+	static_assert(is_vstruct_v<S2>);
+	static_assert(bits<S2>() == 328);
+	for (size_t i = 0; i < 5; i++) {
+		s2.arr[i] = 0;
+	}
+	s2.c = 0;
+	EXPECT_EQ(s2.arr.v[0].v[0], 0);
+	EXPECT_EQ(s2.arr.v[1].v[0], 0);
+	EXPECT_EQ(s2.arr.v[2].v[0], 0);
+	EXPECT_EQ(s2.arr.v[3].v[0], 0);
+	EXPECT_EQ(s2.arr.v[4].v[0], 0);
+}
