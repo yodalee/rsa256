464bfb45875f6a4372196562c54fbc82508c890b
Yoda Lee <yodalee@users.noreply.github.com>
2023-06-20 00:54:56 +0800

    Fix pipeline and RSA module (#135)
    * Fix pipeline implementation
    * Fix RSA error due to pipeline change

--- verilog/Pipeline.sv
+++ verilog/Pipeline.sv
@@ -11,13 +11,13 @@ module Pipeline (
 logic o_valid_w;
 
 // There is valid data if:
-// 1. this cycle push data into register
+// 1. this cycle there is data to be stored
 // 2. there is old data in register and not consumed yet
-assign o_valid_w = o_en || (o_valid && !o_ready);
+assign o_valid_w = i_valid || (o_valid && !o_ready);
 // The module is ready to receive data if:
 // 1. No data need to write out, or
 // 2. The next module is ready to receive data
-assign i_ready = !o_valid && o_ready;
+assign i_ready = !o_valid || o_ready;
 assign o_en = i_ready && i_valid;
 
 always_ff @( posedge clk or negedge rst_n ) begin
