63b3a2cb46280762edf4ad6f773fe38141d85728
Yoda Lee <yodalee@users.noreply.github.com>
2023-07-18 21:41:17 +0800

    Read/Write port support for VArray (#142)
    * Add test for read_port/write_port for varray
    
    * Pass one more test
    
    ---------
    
    Co-authored-by: Yu-Sheng Lin <johnjohnlys@gmail.com>

--- vtuber/bridge/verilator/verilator_assign.h
+++ vtuber/bridge/verilator/verilator_assign.h
@@ -2,6 +2,8 @@
 
 #include "verilated.h"
 #include "verilog/dtype/vint.h"
+#include "verilog/operation/bits.h"
+#include "verilog/operation/unpack.h"
 
 namespace verilog {
 namespace verilator {
@@ -31,12 +33,12 @@ void write_port(verilator_wdtype<num_bit> &dst, const vint<false, num_bit> &src)
 
 // read vuint from verilator port
 template <unsigned num_bit>
-void read_port(vint<false, num_bit> &dst, const verilator_dtype<num_bit> &src) {
+void read_port_scalar(vint<false, num_bit> &dst, const verilator_dtype<num_bit> &src) {
   dst = src;
 }
 
 template <unsigned num_bit>
-void read_port(vint<false, num_bit> &dst, const verilator_wdtype<num_bit> &src) {
+void read_port_scalar(vint<false, num_bit> &dst, const verilator_wdtype<num_bit> &src) {
   const size_t verilator_num_word = to_verilator_num_word(num_bit);
   for (size_t i = 0; i < (verilator_num_word/2); ++i) {
     dst.v[i] = (
@@ -46,6 +48,18 @@ void read_port(vint<false, num_bit> &dst, const verilator_wdtype<num_bit> &src) 
   }
   if constexpr ((verilator_num_word%2) == 1) {
     dst.v[verilator_num_word/2] = uint64_t(src.m_storage[verilator_num_word-1]);
+  }
+}
+
+template <typename Dst, typename Src>
+void read_port(Dst& dst, Src& src) {
+  static_assert(is_dtype_v<Dst>, "Only works for Dst is a verilog type");
+  if constexpr (is_dtype_of_id_v<Dst, DTYPE_VINT>) {
+    read_port_scalar(dst, src);
+  } else {
+    vint<false, verilog::bits_v<Dst>> tmp;
+    read_port_scalar(tmp, src);
+    unpack(dst, tmp);
   }
 }
 
