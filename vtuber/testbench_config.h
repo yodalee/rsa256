b74346e78e10409216ed3cccaf2af259bd28a94d
Yoda Lee <lc85301@gmail.com>
2023-08-24 00:29:39 +0800

    Add fileinfo and their parser

--- vtuber/testbench_config.h
+++ vtuber/testbench_config.h
@@ -5,8 +5,17 @@
 #include <memory>
 #include <vector>
 
+enum class FileType { kBinary, kHexString };
+
+struct FileInfo {
+  FileType type;
+  ::std::string path;
+};
+
 struct TestbenchConfig {
   bool dump;
-  std::unique_ptr<BoolPattern> input_pattern;
-  std::unique_ptr<BoolPattern> output_pattern;
+  ::std::unique_ptr<BoolPattern> input_pattern;
+  ::std::unique_ptr<BoolPattern> output_pattern;
+  ::std::vector<FileInfo> input_files;
+  ::std::vector<FileInfo> output_files;
 };
