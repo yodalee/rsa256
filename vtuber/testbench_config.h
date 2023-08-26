f84fffd61871d93fe559d8dceee4f83f4896b834
Yoda Lee <yodalee@users.noreply.github.com>
2023-08-26 23:30:23 +0800

    More yaml configuration (#153)
    * Add optional bool pattern in testbench config
    
    * Add fileinfo and their parser
    
    * Add test for testbench_config inputs/outputs

--- vtuber/testbench_config.h
+++ vtuber/testbench_config.h
@@ -1,5 +1,21 @@
 #pragma once
 
+#include "abstract_random.h"
+
+#include <memory>
+#include <vector>
+
+enum class FileType { kBinary, kHexString };
+
+struct FileInfo {
+  FileType type;
+  ::std::string path;
+};
+
 struct TestbenchConfig {
   bool dump;
+  ::std::unique_ptr<BoolPattern> input_pattern;
+  ::std::unique_ptr<BoolPattern> output_pattern;
+  ::std::vector<FileInfo> input_files;
+  ::std::vector<FileInfo> output_files;
 };
--- vtuber/testbench_config_yaml.cpp
+++ vtuber/testbench_config_yaml.cpp
@@ -1,11 +1,51 @@
+#include "abstract_random.h"
+#include "abstract_random_yaml.h"
 #include "testbench_config.h"
 #include <glog/logging.h>
 #include <iostream>
 #include <yaml-cpp/yaml.h>
 using namespace std;
 
+void operator>>(const YAML::Node &node, FileInfo &info) {
+  string typ = node["type"].as<::std::string>();
+  if (typ == "binary") {
+    info.type = FileType::kBinary;
+  } else if (typ == "hexstr") {
+    info.type = FileType::kHexString;
+  } else {
+    throw YAML::ParserException(node["type"].Mark(), "Invalid File Type");
+  }
+  info.path = node["path"].as<::std::string>();
+}
+
 void operator>>(const YAML::Node &node, TestbenchConfig &config) {
   config.dump = node["dump"].as<bool>();
+  // parse input, output random pattern
+  if (const YAML::Node &pattern_node = node["input_pattern"]) {
+    config.input_pattern =
+        unique_ptr<BoolPattern>(random_factory::FromYamlNode(pattern_node));
+  } else {
+    config.input_pattern = {};
+  }
+  if (const YAML::Node &pattern_node = node["output_pattern"]) {
+    config.output_pattern =
+        unique_ptr<BoolPattern>(random_factory::FromYamlNode(pattern_node));
+  } else {
+    config.output_pattern = {};
+  }
+
+  // parse input/output files
+  FileInfo info;
+  const YAML::Node &in_node = node["inputs"];
+  for (YAML::const_iterator it = in_node.begin(); it != in_node.end(); ++it) {
+    *it >> info;
+    config.input_files.push_back(info);
+  }
+  const YAML::Node &out_node = node["outputs"];
+  for (YAML::const_iterator it = out_node.begin(); it != out_node.end(); ++it) {
+    *it >> info;
+    config.output_files.push_back(info);
+  }
 }
 
 namespace testbench_config {
