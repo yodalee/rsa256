16e2d431c8635bb97694008abf23f9cf6be2dac6
Yu-Sheng Lin <johnjohnlys@media.ee.ntu.edu.tw>
2023-09-09 20:59:05 +0800

    Change the endianess of array pack/unpack (#156)
    * Change the endianess of array pack/unpack
    * Fix tests for file read/write

--- vtuber/verilog/dtype/varray_test.cpp
+++ vtuber/verilog/dtype/varray_test.cpp
@@ -60,12 +60,12 @@ TEST(TestVerilogArray, Basic) {
 
 TEST(TestVerilogArray, Pack) {
   Arr2 v3x2x3;
-  v3x2x3[0][0] = 0;
-  v3x2x3[0][1] = 1;
-  v3x2x3[0][2] = 2;
-  v3x2x3[1][0] = 3;
-  v3x2x3[1][1] = 4;
-  v3x2x3[1][2] = 5;
+  v3x2x3[1][2] = 0;
+  v3x2x3[1][1] = 1;
+  v3x2x3[1][0] = 2;
+  v3x2x3[0][2] = 3;
+  v3x2x3[0][1] = 4;
+  v3x2x3[0][0] = 5;
   vuint<18> tmps = pack(v3x2x3);
   EXPECT_EQ(tmps, 0012345);
 }
@@ -77,21 +77,21 @@ TEST(TestVerilogArray, Unpack) {
   {
     varray<vuint<32>, 8> a32_8;
     unpack(a32_8, v256);
-    EXPECT_EQ(a32_8[0].value(), 0xD1798A3C);
-    EXPECT_EQ(a32_8[1].value(), 0x8326770B);
-    EXPECT_EQ(a32_8[2].value(), 0x91643739);
-    EXPECT_EQ(a32_8[3].value(), 0x7AB540E3);
-    EXPECT_EQ(a32_8[4].value(), 0xD21345C7);
-    EXPECT_EQ(a32_8[5].value(), 0x539C5996);
-    EXPECT_EQ(a32_8[6].value(), 0xF9ADDDCF);
-    EXPECT_EQ(a32_8[7].value(), 0xA45F1DFE);
+    EXPECT_EQ(a32_8[7].value(), 0xD1798A3C);
+    EXPECT_EQ(a32_8[6].value(), 0x8326770B);
+    EXPECT_EQ(a32_8[5].value(), 0x91643739);
+    EXPECT_EQ(a32_8[4].value(), 0x7AB540E3);
+    EXPECT_EQ(a32_8[3].value(), 0xD21345C7);
+    EXPECT_EQ(a32_8[2].value(), 0x539C5996);
+    EXPECT_EQ(a32_8[1].value(), 0xF9ADDDCF);
+    EXPECT_EQ(a32_8[0].value(), 0xA45F1DFE);
   }
   {
     varray<vuint<64>, 4> a64_4;
     unpack(a64_4, v256);
-    EXPECT_EQ(a64_4[0].value(), 0xD1798A3C8326770B);
-    EXPECT_EQ(a64_4[1].value(), 0x916437397AB540E3);
-    EXPECT_EQ(a64_4[2].value(), 0xD21345C7539C5996);
-    EXPECT_EQ(a64_4[3].value(), 0xF9ADDDCFA45F1DFE);
+    EXPECT_EQ(a64_4[3].value(), 0xD1798A3C8326770B);
+    EXPECT_EQ(a64_4[2].value(), 0x916437397AB540E3);
+    EXPECT_EQ(a64_4[1].value(), 0xD21345C7539C5996);
+    EXPECT_EQ(a64_4[0].value(), 0xF9ADDDCFA45F1DFE);
   }
 }
