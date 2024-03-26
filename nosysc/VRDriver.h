c74b9890c12fbbe410218807440a924c075bb6a9
Yoda Lee <lc85301@gmail.com>
2024-03-26 22:56:25 +0800

    Allow switch random policy in driver and monitor

--- nosysc/VRDriver.h
+++ nosysc/VRDriver.h
@@ -1,17 +1,20 @@
 #pragma once
 
+#include "abstract_random.h"
 #include "nosysc.h"
 #include <iostream>
+#include <memory>
 #include <random>
 
 namespace nosysc {
 
-::std::bernoulli_distribution dist1(0.5);
-::std::default_random_engine reng1;
-
 struct Driver {
+  Driver(BoolPattern *new_reng = nullptr) { SetRandomValidPolicy(new_reng); }
+  ::std::unique_ptr<BoolPattern> reng;
   ValidReadyOutIf<unsigned> *o;
   unsigned counter = 0;
+
+  void SetRandomValidPolicy(BoolPattern *new_reng) { reng.reset(new_reng); }
 
   void ClockedBy(Clock &clk) {
     clk.AddIfDependency([this]() { always_comb(); }, {o});
@@ -19,7 +22,7 @@ struct Driver {
 
   void always_comb() {
     ::std::cout << "Driver" << ::std::endl;
-    if (o->is_writeable() and dist1(reng1)) {
+    if (o->is_writeable() and (not reng or reng->operator()())) {
       o->write(counter);
       ::std::cout << "Driver write" << ::std::endl;
       counter++;
--- nosysc/VRMonitor.h
+++ nosysc/VRMonitor.h
@@ -1,17 +1,19 @@
-
 #pragma once
 
+#include "abstract_random.h"
 #include "nosysc.h"
 #include <iostream>
+#include <memory>
 #include <random>
 
 namespace nosysc {
 
-::std::bernoulli_distribution dist2(0.5);
-::std::default_random_engine reng2;
-
 struct Monitor {
+  Monitor(BoolPattern *new_reng = nullptr) { SetRandomValidPolicy(new_reng); }
+  ::std::unique_ptr<BoolPattern> reng;
   ValidReadyInIf<unsigned> *i;
+
+  void SetRandomValidPolicy(BoolPattern *new_reng) { reng.reset(new_reng); }
 
   void ClockedBy(Clock &clk) {
     clk.AddIfDependency([this]() { always_comb(); }, {i});
@@ -19,7 +21,7 @@ struct Monitor {
 
   void always_comb() {
     ::std::cout << "Monitor" << ::std::endl;
-    if (i->is_readable() and dist2(reng2)) {
+    if (i->is_readable() and (not reng or reng->operator()())) {
       ::std::cout << "Read " << i->read() << ::std::endl;
     }
   }
